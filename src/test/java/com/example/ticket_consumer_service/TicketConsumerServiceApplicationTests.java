087ce21a42f951cd1f870230ea805a1d4216c91e
almeida.desenvolve@gmail.com <almeida.desenvolve@gmail.com>
2026-07-19 17:43:44 -0300

    feat: initial

---
+++ src/test/java/com/example/ticket_consumer_service/TicketConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticket_consumer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
